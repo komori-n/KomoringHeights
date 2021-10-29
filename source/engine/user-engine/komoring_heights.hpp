eabe409b957e8077d48705bd3587b2e5193a05b8
komori-n <ikamat.kmr@gmail.com>
2021-10-29 19:13:15 +0900

    refactor: split up the `MoveSelector` class

--- source/engine/user-engine/komoring_heights.hpp
+++ source/engine/user-engine/komoring_heights.hpp
@@ -10,6 +10,7 @@
 #include <vector>
 
 #include "../../types.h"
+#include "move_selector.hpp"
 #include "transposition_table.hpp"
 #include "ttentry.hpp"
 
@@ -18,107 +19,6 @@ namespace internal {
 /// 詰将棋の最大手数。ミクロコスモス（1525手詰）より十分大きな値を設定する
 constexpr Depth kMaxNumMateMoves = 3000;
 
-/// 探索中に子局面の情報を一時的に覚えておくための構造体
-struct ChildNodeCache {
-  LookUpQuery query;
-  TTEntry* entry;
-  Move move;
-  PnDn min_n, sum_n;
-  std::uint32_t generation;
-  int value;
-};
-
-/**
- * @brief 次に探索すべき子ノードを選択する
- *
- * 詰将棋探索では、TranspositionTable の LookUp に最も時間がかかる。そのため、TTEntry をキャッシュして
- * 必要最低限の回数だけ LookUp を行うようにすることで高速化を図る。
- *
- * OrNode と AndNode でコードの共通化を図るために、pn/dn ではなく min_n/sum_n を用いる。
- *
- *     OrNode:  min_n=pn, sum_n=dn
- *     AndNode: min_n=dn, sum_n=pn
- *
- * このようにすることで、いずれの場合の子局面選択も「min_n が最小となる子局面を選択する」という処理に帰着できる。
- *
- * @note コンストラクタ時点で子局面の TTEntry が存在しない場合、エントリの作成は実際の探索まで遅延される。
- * その場合 ChildNodeCache::entry が nullptr になる可能性がある。
- */
-template <bool kOrNode>
-class MoveSelector {
- public:
-  MoveSelector() = delete;
-  MoveSelector(const MoveSelector&) = delete;
-  MoveSelector(MoveSelector&&) noexcept = default;  ///< vector で領域確保させるために必要
-  MoveSelector& operator=(const MoveSelector&) = delete;
-  MoveSelector& operator=(MoveSelector&&) = delete;
-  ~MoveSelector() = default;
-
-  MoveSelector(const Position& n, TranspositionTable& tt, Depth depth);
-
-  /// 子局面の entry の再 LookUp を行い、現曲面の pn/dn を計算し直す
-  void Update(std::unordered_set<Key>& parents);
-
-  bool empty() const;
-
-  PnDn Pn() const;
-  PnDn Dn() const;
-  /// 現局面は千日手による不詰か
-  bool IsRepetitionDisproven() const;
-
-  /// 現局面に対する反証駒を計算する
-  Hand ProofHand() const;
-  /// 現局面に対する反証駒を計算する
-  Hand DisproofHand() const;
-
-  /// 現時点で最善の手を返す
-  Move FrontMove() const;
-  /// 現時点で最善の手に対する TTEntry を返す
-  /// @note entry が nullptr のために関数内で LookUp する可能性があるので、const メンバ関数にはできない
-  TTEntry* FrontTTEntry();
-  /// 現時点で最善の手に対する LookUpQuery を返す
-  const LookUpQuery& FrontLookUpQuery() const;
-
-  /**
-   * @brief 子局面の探索の (pn, dn) のしきい値を計算する
-   *
-   * @param thpn 現局面の pn のしきい値
-   * @param thdn 現曲面の dn のしきい値
-   * @return std::pair<PnDn, PnDn> 子局面の (pn, dn) のしきい値の組
-   */
-  std::pair<PnDn, PnDn> ChildThreshold(PnDn thpn, PnDn thdn) const;
-
- private:
-  /**
-   * @brief 子局面同士を探索したい順に並べ替えるための比較演算子
-   *
-   * 以下の順で判定を行いエントリを並べ替える
-   *   1. min_n が小さい順
-   *   2. generation が小さい順（最も昔に探索したものから順）
-   *   3. move.value が小さい順
-   */
-  bool Compare(const ChildNodeCache& lhs, const ChildNodeCache& rhs) const;
-
-  /// 現局面の min_n を返す
-  PnDn MinN() const;
-  /// 現局面の sum_n を返す
-  PnDn SumN() const;
-  /// 子局面のうち 2 番目に小さい min_n を返す
-  PnDn SecondMinN() const;
-  /// children_[0] を除いた sum_n を返す
-  PnDn SumNExceptFront() const;
-  /// 現時点で最善の手の持ち駒を返す
-  /// @note 詰み／不詰の局面では証明駒／反証駒を返す（現局面の持ち駒とは一致しない可能性がある）
-  Hand FrontHand() const;
-
-  const Position& n_;
-  TranspositionTable& tt_;
-  const Depth depth_;
-
-  std::array<ChildNodeCache, kMaxCheckMovesPerNode> children_;
-  std::size_t children_len_;
-  PnDn sum_n_;
-};
 }  // namespace internal
 
 /// df-pn探索の本体
@@ -174,8 +74,8 @@ class DfPnSearcher {
 
   TranspositionTable tt_{};
   /// Selector を格納する領域。stack に積むと stackoverflow になりがちなため
-  std::vector<internal::MoveSelector<true>> or_selectors_{};
-  std::vector<internal::MoveSelector<false>> and_selectors_{};
+  std::vector<MoveSelector<true>> or_selectors_{};
+  std::vector<MoveSelector<false>> and_selectors_{};
 
   std::atomic_bool* stop_{nullptr};
   std::uint64_t searched_node_{};
---
+++ source/engine/user-engine/move_selector.cpp
@@ -0,0 +1,219 @@
+#include "move_selector.hpp"
+
+#include "move_picker.hpp"
+#include "node_travels.hpp"
+#include "proof_hand.hpp"
+
+namespace komori {
+
+template <bool kOrNode>
+MoveSelector<kOrNode>::MoveSelector(const Position& n, TranspositionTable& tt, Depth depth)
+    : n_{n}, tt_(tt), depth_{depth}, children_len_{0}, sum_n_{0} {
+  // 各子局面の LookUp を行い、min_n の昇順になるように手を並べ替える
+  auto move_picker = MovePicker<kOrNode, true>{n};
+  for (const auto& move : move_picker) {
+    auto& child = children_[children_len_++];
+    child.move = move.move;
+    child.value = move.value;
+
+    child.query = tt.GetChildQuery<kOrNode>(n, child.move, depth_ + 1);
+    auto entry = child.query.LookUpWithoutCreation();
+    child.min_n = kOrNode ? entry->Pn() : entry->Dn();
+    child.sum_n = kOrNode ? entry->Dn() : entry->Pn();
+    child.generation = entry->StateGeneration();
+    if (child.query.DoesStored(entry)) {
+      child.entry = entry;
+    } else {
+      child.entry = nullptr;
+    }
+
+    sum_n_ = std::min(sum_n_ + child.sum_n, kInfinitePnDn);
+    // 勝ちになる手があるならこれ以上調べる必要はない
+    if (child.min_n == 0) {
+      break;
+    }
+  }
+
+  std::sort(children_.begin(), children_.begin() + children_len_,
+            [this](const auto& lhs, const auto& rhs) { return Compare(lhs, rhs); });
+}
+
+template <bool kOrNode>
+void MoveSelector<kOrNode>::Update(std::unordered_set<Key>& parents) {
+  // 各子局面のエントリを更新する
+  for (std::size_t i = 0; i < std::min(children_len_, std::size_t{2}); ++i) {
+    auto& child = children_[i];
+    auto* entry = child.query.RefreshWithoutCreation(child.entry);
+    if (child.query.DoesStored(entry)) {
+      child.entry = entry;
+    } else {
+      child.entry = nullptr;
+    }
+
+    auto old_sum_n = child.sum_n;
+    child.min_n = kOrNode ? entry->Pn() : entry->Dn();
+    child.sum_n = kOrNode ? entry->Dn() : entry->Pn();
+    child.generation = entry->StateGeneration();
+
+    sum_n_ = std::min(sum_n_ - old_sum_n + child.sum_n, kInfinitePnDn);
+  }
+
+  std::sort(children_.begin(), children_.begin() + children_len_,
+            [this](const auto& lhs, const auto& rhs) { return Compare(lhs, rhs); });
+
+  if (MinN() == 0) {
+    // MinN() == 0 ではないエントリは今後使う可能性が薄いので削除候補にする
+    for (std::size_t i = 1; i < children_len_; ++i) {
+      auto& child = children_[i];
+      auto* entry = child.entry;
+      if (entry == nullptr) {
+        continue;
+      }
+
+      entry = child.query.RefreshWithoutCreation(entry);
+      if (entry->Pn() != 0 && entry->Dn() != 0) {
+        MarkDeleteCandidates<kOrNode>(tt_, const_cast<Position&>(n_), depth_, parents, child.query, entry);
+      }
+    }
+  }
+}
+
+template <bool kOrNode>
+bool MoveSelector<kOrNode>::empty() const {
+  return children_len_ == 0;
+}
+
+template <bool kOrNode>
+PnDn MoveSelector<kOrNode>::Pn() const {
+  return kOrNode ? MinN() : SumN();
+}
+
+template <bool kOrNode>
+PnDn MoveSelector<kOrNode>::Dn() const {
+  return kOrNode ? SumN() : MinN();
+}
+
+template <bool kOrNode>
+bool MoveSelector<kOrNode>::IsRepetitionDisproven() const {
+  if (children_len_ == 0) {
+    return false;
+  }
+
+  if constexpr (kOrNode) {
+    // 1 つでも千日手に向かう手があるなら、この局面はそれが原因で不詰になっているかもしれない
+    return GetState(children_[children_len_ - 1].generation) == kRepetitionDisprovenState;
+  } else {
+    return GetState(children_[0].generation) == kRepetitionDisprovenState;
+  }
+}
+
+template <bool kOrNode>
+Hand MoveSelector<kOrNode>::ProofHand() const {
+  if constexpr (kOrNode) {
+    return BeforeHand(n_, FrontMove(), FrontHand());
+  } else {
+    // 子局面の証明駒の極小集合を計算する
+    HandSet proof_hand = HandSet::Zero();
+    for (std::size_t i = 0; i < children_len_; ++i) {
+      const auto& child = children_[i];
+      proof_hand |= child.query.GetHand();
+    }
+    proof_hand &= n_.hand_of(~n_.side_to_move());
+    return AddIfHandGivesOtherEvasions(n_, proof_hand.Get());
+  }
+}
+
+template <bool kOrNode>
+Hand MoveSelector<kOrNode>::DisproofHand() const {
+  if constexpr (kOrNode) {
+    // 子局面の反証駒の極大集合を計算する
+    HandSet disproof_hand = HandSet::Full();
+    for (std::size_t i = 0; i < children_len_; ++i) {
+      const auto& child = children_[i];
+      disproof_hand &= BeforeHand(n_, child.move, child.query.GetHand());
+    }
+    disproof_hand |= n_.hand_of(n_.side_to_move());
+    return RemoveIfHandGivesOtherChecks(n_, disproof_hand.Get());
+  } else {
+    return FrontHand();
+  }
+}
+
+template <bool kOrNode>
+Move MoveSelector<kOrNode>::FrontMove() const {
+  // empty かどうかは呼び出し側がチェックすべきなのでここではしない
+  return children_[0].move;
+}
+
+template <bool kOrNode>
+TTEntry* MoveSelector<kOrNode>::FrontTTEntry() {
+  // empty かどうかは呼び出し側がチェックすべきなのでここではしない
+  auto& child = children_[0];
+  if (child.entry == nullptr) {
+    // entry がないなら作る
+    child.entry = child.query.LookUpWithCreation();
+  }
+  return child.entry;
+}
+
+template <bool kOrNode>
+const LookUpQuery& MoveSelector<kOrNode>::FrontLookUpQuery() const {
+  // empty かどうかは呼び出し側がチェックすべきなのでここではしない
+  return children_[0].query;
+}
+
+template <bool kOrNode>
+std::pair<PnDn, PnDn> MoveSelector<kOrNode>::ChildThreshold(PnDn thpn, PnDn thdn) const {
+  auto thmin_n = kOrNode ? thpn : thdn;
+  auto thsum_n = kOrNode ? thdn : thpn;
+  auto child_thmin_n = std::min(thmin_n, SecondMinN() + 1);
+  auto child_thsum_n = std::min(thsum_n - SumNExceptFront(), kInfinitePnDn);
+
+  return kOrNode ? std::make_pair(child_thmin_n, child_thsum_n) : std::make_pair(child_thsum_n, child_thmin_n);
+}
+
+template <bool kOrNode>
+bool MoveSelector<kOrNode>::Compare(const ChildNodeCache& lhs, const ChildNodeCache& rhs) const {
+  if (lhs.min_n != rhs.min_n) {
+    return lhs.min_n < rhs.min_n;
+  }
+  if (lhs.generation != rhs.generation) {
+    return lhs.generation < rhs.generation;
+  }
+  return lhs.value < rhs.value;
+}
+
+template <bool kOrNode>
+PnDn MoveSelector<kOrNode>::MinN() const {
+  return children_len_ > 0 ? children_[0].min_n : kInfinitePnDn;
+}
+
+template <bool kOrNode>
+PnDn MoveSelector<kOrNode>::SumN() const {
+  return sum_n_;
+}
+
+template <bool kOrNode>
+PnDn MoveSelector<kOrNode>::SecondMinN() const {
+  return children_len_ > 1 ? children_[1].min_n : kInfinitePnDn;
+}
+
+template <bool kOrNode>
+PnDn MoveSelector<kOrNode>::SumNExceptFront() const {
+  return sum_n_ - children_[0].sum_n;
+}
+
+template <bool kOrNode>
+Hand MoveSelector<kOrNode>::FrontHand() const {
+  auto& child = children_[0];
+  if (child.entry != nullptr) {
+    return child.entry->ProperHand(child.query.GetHand());
+  } else {
+    auto* entry = child.query.LookUpWithoutCreation();
+    return entry->ProperHand(child.query.GetHand());
+  }
+}
+
+template class MoveSelector<false>;
+template class MoveSelector<true>;
+}  // namespace komori
