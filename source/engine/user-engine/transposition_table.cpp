6a5a4b7a9e940e11e166f33c1de929c5781eaf67
komori-n <ikamat.kmr@gmail.com>
2021-11-02 23:20:24 +0900

    fix: fix a possible infinite loop

--- source/engine/user-engine/transposition_table.cpp
+++ source/engine/user-engine/transposition_table.cpp
@@ -68,8 +68,9 @@ bool LookUpQuery::DoesStored(TTEntry* entry) const {
 }
 
 bool LookUpQuery::IsValid(TTEntry* entry) const {
-  return cluster_->DoesContain(entry) && hash_high_ == entry->HashHigh() && entry->ExactOrDeducable(hand_, depth_) &&
-         !entry->IsMaybeRepetitionNode();
+  return cluster_->DoesContain(entry) && hash_high_ == entry->HashHigh() &&
+         ((entry->ExactOrDeducable(hand_, depth_) && !entry->IsMaybeRepetitionNode()) ||
+          entry->IsRepetitionNode() && entry->CheckRepetition(path_key_));
 }
 
 TranspositionTable::TranspositionTable(void) = default;
