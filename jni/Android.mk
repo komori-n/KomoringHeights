aa8e8fe48d3bf4ae54871c267770d79b57c9d148
komori-n <ikamat.kmr@gmail.com>
2021-11-03 16:55:44 +0900

    fix: fix compile error for android binary

--- jni/Android.mk
+++ jni/Android.mk
@@ -226,6 +226,7 @@ LOCAL_SRC_FILES += \
   ../source/engine/user-engine/proof_hand.cpp \
   ../source/engine/user-engine/node_travels.cpp \
   ../source/engine/user-engine/move_selector.cpp \
+  ../source/engine/user-engine/path_keys.cpp \
   ../source/engine/user-engine/transposition_table.cpp
 endif
 
