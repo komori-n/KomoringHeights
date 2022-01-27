61c532990174f96c3c75ce6e01802a447bdf5f2d
komori-n <ikamat.kmr@gmail.com>
2022-01-27 22:02:20 +0900

    fix: fix missing files

---
+++ source/engine/user-engine/engine_option.cpp
@@ -0,0 +1,82 @@
+#include "engine_option.hpp"
+
+#include <cmath>
+#include <limits>
+
+namespace komori {
+namespace {
+template <typename T>
+T ReadValue(const USI::OptionsMap& o, const std::string& key);
+
+template <typename N>
+N ReadValue(const USI::OptionsMap& o, const std::string& key) {
+  static_assert(std::is_integral_v<N>);
+
+  if (auto itr = o.find(key); itr != o.end()) {
+    if (auto val = static_cast<N>(itr->second)) {
+      return val;
+    }
+  }
+
+  return std::numeric_limits<N>::max();
+}
+
+template <>
+bool ReadValue<bool>(const USI::OptionsMap& o, const std::string& key) {
+  auto val = ReadValue<int>(o, key);
+  return val != 0;
+}
+}  // namespace
+
+void EngineOption::Init(USI::OptionsMap& o) {
+  o["DepthLimit"] << USI::Option(0, 0, INT_MAX);
+  o["NodesLimit"] << USI::Option(0, 0, INT64_MAX);
+  o["PvInterval"] << USI::Option(1000, 0, 1000000);
+
+  o["PostSearchCount"] << USI::Option(400, 0, INT64_MAX);
+
+  o["RootIsAndNodeIfChecked"] << USI::Option(true);
+  o["YozumePrintLevel"] << USI::Option(2, 0, 3);
+
+#if defined(USE_DEEP_DFPN)
+  o["DeepDfpnPerMile"] << USI::Option(5, 0, 10000);
+  o["DeepDfpnMaxVal"] << USI::Option(1000000, 1, INT64_MAX);
+#endif  // defined(USE_DEEP_DFPN)
+}
+
+void EngineOption::Reload(const USI::OptionsMap& o) {
+  hash_mb = ReadValue<std::uint64_t>(o, "USI_Hash");
+  threads = ReadValue<int>(o, "Threads");
+
+  depth_limit = ReadValue<Depth>(o, "DepthLimit");
+  nodes_limit = ReadValue<std::uint64_t>(o, "NodesLimit");
+
+  pv_interval = ReadValue<std::uint64_t>(o, "PvInterval");
+  if (pv_interval == std::numeric_limits<std::uint64_t>::max()) {
+    pv_interval = 0;
+  }
+  post_search_count = ReadValue<std::uint64_t>(o, "PostSearchCount");
+
+  root_is_and_node_if_checked = ReadValue<bool>(o, "RootIsAndNodeIfChecked");
+
+  auto yozume_level_int = ReadValue<int>(o, "YozumePrintLevel");
+  if (static_cast<int>(YozumeVerboseLevel::kBegin) <= yozume_level_int &&
+      yozume_level_int < static_cast<int>(YozumeVerboseLevel::kEnd)) {
+    yozume_print_level = static_cast<YozumeVerboseLevel>(yozume_level_int);
+  } else {
+    yozume_print_level = YozumeVerboseLevel::kNone;
+  }
+
+#if defined(USE_DEEP_DFPN)
+  if (auto val = ReadValue<int>(o, "DeepDfpnPerMile"); val != std::numeric_limits<int>::max()) {
+    deep_dfpn_e_ = 0.001 * val + 1.0;
+    auto max = ReadValue<int>(o, "DeepDfpnMaxVal");
+    deep_dfpn_d_ = static_cast<Depth>(std::log(static_cast<double>(max)) / std::log(e));
+  } else {
+    deep_dfpn_d_ = 0;
+    deep_dfpn_e_ = 1.0;
+  }
+  deep_dfpn_d_ = ReadValue<double>(o, "DeepDfpnMaxVal");
+#endif  // if defined(USE_DEEP_DFPN)
+}
+}  // namespace komori
---
+++ source/engine/user-engine/engine_option.hpp
@@ -0,0 +1,46 @@
+#ifndef ENGINE_OPTION_HPP_
+#define ENGINE_OPTION_HPP_
+
+#include "../../usi.h"
+#include "typedefs.hpp"
+
+namespace komori {
+
+enum class YozumeVerboseLevel {
+  kNone = 0,
+  kOnlyYozume,
+  kYozumeAndUnknown,
+  kAll,
+  // 範囲判定用
+  kEnd,
+  kBegin = 0,
+};
+
+inline bool operator<(YozumeVerboseLevel lhs, YozumeVerboseLevel rhs) {
+  return static_cast<int>(lhs) < static_cast<int>(rhs);
+}
+
+struct EngineOption {
+  std::uint64_t hash_mb;
+  int threads;
+
+  Depth depth_limit;
+  std::uint64_t nodes_limit;
+
+  std::uint64_t pv_interval;
+  std::uint64_t post_search_count;
+
+  bool root_is_and_node_if_checked;
+  YozumeVerboseLevel yozume_print_level;
+
+#if defined(USE_DEEP_DFPN)
+  Depth deep_dfpn_d_;
+  double deep_dfpn_e_;
+#endif
+
+  void Init(USI::OptionsMap& o);
+  void Reload(const USI::OptionsMap& o);
+};
+}  // namespace komori
+
+#endif  // ENGINE_OPTION_HPP_
