35d1c11a0c5ef70f9e5eeaeba2960908524d622e
komori-n <ikamat.kmr@gmail.com>
2022-08-17 22:50:19 +0900

    feat: add documentation for path_key

--- source/engine/user-engine/path_keys.hpp
+++ source/engine/user-engine/path_keys.hpp
@@ -12,7 +12,11 @@ inline HASH_KEY g_dropped_pr[PIECE_HAND_NB][komori::kDepthMax];
 inline HASH_KEY g_stolen_pr[PIECE_HAND_NB][komori::kDepthMax];
 }  // namespace detail
 
-/// ハッシュを初期化する
+/**
+ * @brief 経路ハッシュのテーブルを初期化する。
+ *
+ * 探索開始前に1回だけ呼び出す必要がある。
+ */
 inline void PathKeyInit() {
   using detail::g_dropped_pr;
   using detail::g_move_from;
@@ -41,8 +45,17 @@ inline void PathKeyInit() {
   }
 }
 
-/// 現在の path_key と手 move から1手後の path_key を計算する。値は深さ depth 依存。
-inline Key PathKeyAfter(Key path_key, Move move, Depth depth) {
+/**
+ * @brief 現在の `path_key` と手 `move` から1手後の `path_key` を計算する。
+ * @param[in] path_key 現在の経路ハッシュ値
+ * @param[in] move     次の手
+ * @param[in] depth    現在の探索深さ
+ * @return Key `move` 後の経路ハッシュ値
+ *
+ * 深さ `depth` により経路ハッシュ値が異なる。`depth` の値に応じてハッシュ値を変えることで、手順が前後して同じ局面に
+ * 至る経路の間でハッシュ値がかぶらないようにしている。
+ */
+constexpr inline Key PathKeyAfter(Key path_key, Move move, Depth depth) {
   const auto to = to_sq(move);
   path_key ^= detail::g_move_to[to][depth];
   if (is_drop(move)) {
@@ -59,18 +72,46 @@ inline Key PathKeyAfter(Key path_key, Move move, Depth depth) {
   return path_key;
 }
 
-/// 相手の持ち駒 stolen_pr を 1 枚奪った後の path_key を計算する
-inline Key PathKeyAfterSteal(Key path_key, PieceType stolen_pr, Depth depth) {
-  return path_key ^ detail::g_stolen_pr[stolen_pr][depth];
-}
-
-inline Key PathKeyBefore(Key path_key, Move move, Depth depth) {
-  // xor でハッシュを計算しているので、1手進めるのと同じ関数で戻せる
+/**
+ * @brief 1手後の `path_key` と手 `move` から現在の `path_key` を計算する。
+ * @param[in] path_key `move` 後の経路ハッシュ値
+ * @param[in] move     次の手
+ * @param[in] depth    現在の探索深さ
+ * @return `move` 前の経路ハッシュ値
+ *
+ * `depth` は `move` する直前の深さを渡す必要がある。すなわち、以下の2つの関数は互いに逆写像の関係になっている。
+ *
+ * - `PathKeyAfter(・, move, depth)`
+ * - `PathKeyBefore(・, move, depth)`
+ *
+ * @note `PathKeyAfter` は XOR に基づく差分計算をしているため、逆写像も全く同じ関数で実現できる
+ */
+constexpr inline Key PathKeyBefore(Key path_key, Move move, Depth depth) {
   return PathKeyAfter(path_key, move, depth);
 }
 
-inline Key PathKeyAfterGive(Key path_key, PieceType stolen_pr, Depth depth) {
-  return PathKeyAfterSteal(path_key, stolen_pr, depth);
+/**
+ * @brief 相手の持ち駒 `stolen_pr` を1枚奪った後の `path_key` を計算する。
+ * @param[in] path_key   現在の経路ハッシュ値
+ * @param[in] stolen_pr  相手の持ち駒から奪う駒
+ * @param[in] depth      現在の探索深さ
+ * @return 持ち駒を奪った直後のハッシュ値
+ */
+constexpr inline Key PathKeyAfterSteal(Key path_key, PieceType stolen_pr, Depth depth) {
+  return path_key ^ detail::g_stolen_pr[stolen_pr][depth];
+}
+
+/**
+ * @brief 相手に持ち駒 `stolen_pr` を1枚プレゼントした後の `path_key` を計算する。
+ * @param[in] path_key   現在の経路ハッシュ値
+ * @param[in] given_pr   相手にプレゼントする駒
+ * @param[in] depth      現在の探索深さ
+ * @return 持ち駒を渡した直後のハッシュ値
+ *
+ * @note `PathKeyAfterGive` は XOR に基づく差分計算をしているため、逆写像も全く同じ関数で実現できる
+ */
+constexpr inline Key PathKeyAfterGive(Key path_key, PieceType given_pr, Depth depth) {
+  return PathKeyAfterSteal(path_key, given_pr, depth);
 }
 
 }  // namespace komori
