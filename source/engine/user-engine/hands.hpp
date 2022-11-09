d6d0267839f514e49e7e959f44446b40a9d15eb0
komori-n <komori-n@ymail.ne.jp>
2022-11-09 20:53:08 +0900

    refactor: clean up double pawn detection

--- source/engine/user-engine/hands.hpp
+++ source/engine/user-engine/hands.hpp
@@ -182,17 +182,9 @@ inline Hand AddIfHandGivesOtherEvasions(const Position& n, Hand proof_hand) {
   // 駒を持っていれば合駒で詰みを防げたかもしれない（合法手が増えるから）
   for (PieceType pr = PIECE_HAND_ZERO; pr < PIECE_HAND_NB; ++pr) {
     if (pr == PAWN) {
-      bool double_pawn = true;
-      auto bb = between_bb(king_sq, checker_sq);
-      while (bb) {
-        auto sq = bb.pop();
-        if (!(n.pieces(us, PAWN) & file_bb(file_of(sq)))) {
-          double_pawn = false;
-          break;
-        }
-      }
-
-      if (double_pawn) {
+      const auto drop_mask =
+          (us == BLACK) ? pawn_drop_mask<BLACK>(n.pieces<BLACK>(PAWN)) : pawn_drop_mask<WHITE>(n.pieces<WHITE>(PAWN));
+      if (!drop_mask.test(between_bb(king_sq, checker_sq))) {
         continue;
       }
     }
