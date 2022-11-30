11b5fd9500376930cbd62a1919d4bf870fdd2e34
komori-n <komori-n@ymail.ne.jp>
2022-12-01 08:17:45 +0900

    fix: reduce lazy expansion penalty

--- source/engine/user-engine/local_expansion.hpp
+++ source/engine/user-engine/local_expansion.hpp
@@ -428,9 +428,9 @@ class LocalExpansion {
     // 例） sfen +P5l2/4+S4/p1p+bpp1kp/6pgP/3n1n3/P2NP4/3P1NP2/2P2S3/3K3L1 b RGSL2Prb2gsl3p 159
     //      1筋の合駒を考える時、玉方が合駒を微妙に変えることで読みの深さを指数関数的に大きくできてしまう
     if (mp_.size() > idx_.size()) {
-      // 後回しにしている手1つにつき 1/4 点減点する。小数点以下は切り捨てするが、計算結果が 1 を下回る場合のみ
+      // 後回しにしている手1つにつき 1/8 点減点する。小数点以下は切り捨てするが、計算結果が 1 を下回る場合のみ
       // 1 に切り上げる。
-      sum_delta += std::max<std::size_t>((mp_.size() - idx_.size()) / 4, 1);
+      sum_delta += std::max<std::size_t>((mp_.size() - idx_.size()) / 8, 1);
     }
 
     return ClampPnDn(sum_delta + max_delta);
@@ -452,7 +452,7 @@ class LocalExpansion {
   PnDn NewThdeltaForBestMove(PnDn thdelta) const {
     PnDn delta_except_best = sum_delta_except_best_;
     if (mp_.size() > idx_.size()) {
-      delta_except_best += std::max<std::size_t>((mp_.size() - idx_.size()) / 4, 1);
+      delta_except_best += std::max<std::size_t>((mp_.size() - idx_.size()) / 8, 1);
     }
 
     if (sum_mask_[idx_[0]]) {
